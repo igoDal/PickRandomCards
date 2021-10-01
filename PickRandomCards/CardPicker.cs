5cd1d334e5834c961d5af75c919c8e3f81590617
IgoDal <igortenn@gmail.com>
2021-10-01 15:07:51 +0200

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        private static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            return pickCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            //get a random number from 1 to 4 (5 is not included)
+            if (value == 1)
+                return "Spades";
+            if (value == 2)
+                return "Hearts";
+            if (value == 3)
+                return "Clubs";
+            return "Diamonds"; //it's like else statement
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) //if it's 1 - return string Ace
+                return "Ace";
+            if (value == 11) // if the value is 11 - return string Jack
+                return "Jack";
+            if (value == 12) //if the value is 12 - return string Queen
+                return "Queen";
+            if (value == 13) //if the value is 13 - return string King
+                return "King";
+            return value.ToString(); //this will return an int value (2-10) AS STRING -> "2" instead 2
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                //string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    int index = card.
+                    string[] cardPicked = new string[] { };
+                    cardPicked = CardPicker.PickSomeCards(cardPicked[]);
+                }
+
+                //Console.WriteLine(numberOfCards);
+            }
+            else
+            {
+                Console.WriteLine("Wrong input! Put a number between 1 and 14!");   
+            }
+        }
+    }
+}
